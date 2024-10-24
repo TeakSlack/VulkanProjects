c111dc6c7bdc4f798fdd6521652a7ffa1fe3405a
Teak Slack <teakislack@gmail.com>
2024-10-24 12:51:35 -0400

    Finish HelloVulkan w/ instance creation, validation layers, & debug messenger

--- HelloVulkan/src/main.cpp
+++ HelloVulkan/src/main.cpp
@@ -1,10 +1,23 @@
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+// Hello Vulkan
+// 
+// Initializes the Vulkan API via creation of an
+// instance, enumerates instance layers and extentions,
+// enables certain validation layers in debug mode for
+// safety, then cleans up Vulkan and quits.
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+
 #include <vulkan/vulkan.hpp>
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
 #include <iostream>
 #include <exception>
 #include <vector>
 
 // The connection between the application and Vulkan API
 vk::Instance instance;
+
+vk::DebugUtilsMessengerEXT debugMessenger;
 
 const std::vector<const char*> VALIDATION_LAYERS =
 {
@@ -17,8 +30,21 @@ const bool ENABLE_VALIDATION_LAYERS = false;
 const bool ENABLE_VALIDATION_LAYERS = true;
 #endif
 
+auto logger = spdlog::stdout_color_mt("logger");
 
-bool checkValidationLayerSupport()
+std::vector<const char*> GetRequiredExtentions()
+{
+	std::vector<const char*> extentions;
+
+	if (ENABLE_VALIDATION_LAYERS)
+	{
+		extentions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME); // VK_EXT_debug_utils
+	}
+
+	return extentions;
+}
+
+bool CheckValidationLayerSupport()
 {
 	// Get all available validation layers
 	std::vector<vk::LayerProperties> validationLayers = vk::enumerateInstanceLayerProperties();
@@ -43,9 +69,62 @@ bool checkValidationLayerSupport()
 	return true;
 }
 
+static VKAPI_ATTR VkBool32 VKAPI_CALL DebugCallback(
+	VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+	VkDebugUtilsMessageTypeFlagsEXT messageType,
+	const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+	void* pUserData
+)
+{
+	switch (messageSeverity)
+	{
+	case VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT:
+		break;
+	case VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT:
+		logger->info(pCallbackData->pMessage);
+		break;
+	case VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT:
+		logger->warn(pCallbackData->pMessage);
+		break;
+	case VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT:
+		logger->error(pCallbackData->pMessage);
+		break;
+	default:
+		break;
+	}
+
+	return VK_FALSE; // Always return false unless testing validation layer itself, triggers exception
+}
+
+vk::DebugUtilsMessengerCreateInfoEXT GetDebugMessengerCreateInfo()
+{
+	return vk::DebugUtilsMessengerCreateInfoEXT(
+		vk::DebugUtilsMessengerCreateFlagsEXT(),
+		vk::DebugUtilsMessageSeverityFlagBitsEXT::eVerbose |
+		vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning |
+		vk::DebugUtilsMessageSeverityFlagBitsEXT::eError,
+		vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral |
+		vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation |
+		vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance,
+		DebugCallback,
+		nullptr
+	);
+}
+
+void SetupDebugMessenger()
+{
+	if (!ENABLE_VALIDATION_LAYERS) return;
+
+	vk::DebugUtilsMessengerCreateInfoEXT createInfo = GetDebugMessengerCreateInfo();
+
+	vk::DispatchLoaderDynamic dldi(instance, vkGetInstanceProcAddr);
+	debugMessenger = instance.createDebugUtilsMessengerEXT(createInfo, nullptr, dldi);
+
+}
+
 void CreateInstance()
 {
-	if (ENABLE_VALIDATION_LAYERS && !checkValidationLayerSupport())
+	if (ENABLE_VALIDATION_LAYERS && !CheckValidationLayerSupport())
 		throw std::runtime_error("Validation layers requested, but not available!");
 
 	vk::ApplicationInfo appInfo
@@ -65,36 +144,44 @@ void CreateInstance()
 
 	};
 
+	// Debug create info to pass to pNext, so we can get diagnostics on instance creation and destruction
+	vk::DebugUtilsMessengerCreateInfoEXT debugCreateInfo{};
+
 	// Include validation layer names, if enabled
 	if (ENABLE_VALIDATION_LAYERS)
 	{
 		createInfo.enabledLayerCount = static_cast<uint32_t>(VALIDATION_LAYERS.size());
 		createInfo.ppEnabledLayerNames = VALIDATION_LAYERS.data();
+		debugCreateInfo = GetDebugMessengerCreateInfo();
+		createInfo.pNext = &debugCreateInfo; // Pass callback only if validation layers are enabled
 	}
 	else
 	{
 		createInfo.enabledLayerCount = 0;
 	}
 
-	// Enumerates and displays available extensions
+	// Enumerates available extensions
 	std::vector<vk::ExtensionProperties> instanceExtensions = vk::enumerateInstanceExtensionProperties();
 
-	std::cout << "Avaliable instance extensions:" << std::endl;
-	for (auto& ext : instanceExtensions)
-	{
-		std::cout << "\t" << ext.extensionName << std::endl;
-	}
+	// Enable the requested extentions
+	auto extentions = GetRequiredExtentions();
+	createInfo.enabledExtensionCount = extentions.size();
+	createInfo.ppEnabledExtensionNames = extentions.data();
 
 	instance = vk::createInstance(createInfo);
 }
 
 void Initialize()
 {
+	logger->set_pattern("[%H:%M:%S %^%l%$]: %v"); // [Hour:Minute:Second Level]: Message
 	CreateInstance();
+	SetupDebugMessenger();
 }
 
 void Cleanup()
 {
+	vk::DispatchLoaderDynamic dldi(instance, vkGetInstanceProcAddr);
+	instance.destroyDebugUtilsMessengerEXT(debugMessenger, nullptr, dldi);
 	instance.destroy();
 }
 
