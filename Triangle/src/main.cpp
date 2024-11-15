c1c1ae7e6513736a78185c6b3c65f8c0ba5f14c1
Teak Slack <teakislack@gmail.com>
2024-11-15 15:53:34 -0500

    Triangle: image views

--- Triangle/src/main.cpp
+++ Triangle/src/main.cpp
@@ -183,7 +183,34 @@ void CreateImageViews()
 
 	for (size_t i = 0; i < swapImageViews.size(); i++)
 	{
+		VkImageViewCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+		createInfo.image = swapImages[i];
+		createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+		createInfo.format = swapFormat;
+		createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+		createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+		createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+		createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+		createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+		createInfo.subresourceRange.baseMipLevel = 0;
+		createInfo.subresourceRange.levelCount = 1;
+		createInfo.subresourceRange.baseArrayLayer = 0;
+		createInfo.subresourceRange.layerCount = 1;
 
+		if (vkCreateImageView(device.device, &createInfo, nullptr, &swapImageViews[i]) != VK_SUCCESS)
+		{
+			logger->error("Failed to create image view!");
+			exit(EXIT_FAILURE);
+		}
+	}
+}
+
+void DestroyImageViews()
+{
+	for (auto imageView : swapImageViews)
+	{
+		vkDestroyImageView(device.device, imageView, nullptr);
 	}
 }
 
@@ -218,12 +245,14 @@ void Run()
 	CreateGLFWWindow();
 	CreateDevice();
 	CreateSwapchain();
+	CreateImageViews();
 
 	while (!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
 	}
 
+	DestroyImageViews();
 	DestroySwapchain();
 	DestroyDevice();
 	DestroyInstance();
